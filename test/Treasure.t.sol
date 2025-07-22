02bdc62aad279cec266db03a94e921afc8eaa025
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2025-07-22 16:19:41 -0400

    Add tests

--- test/Treasure.t.sol
+++ test/Treasure.t.sol
@@ -1,17 +1,86 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.30;
 
 import {Test} from "forge-std/Test.sol";
 import {Treasure} from "../src/Treasure.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TreasureTest is Test {
     Treasure public treasure;
+    MockCollectible public collectible;
+    MockERC20 public erc20;
+
+    address public user1 = makeAddr("user1");
+    address public user2 = makeAddr("user2");
 
     function setUp() public {
-        treasure = new Treasure(address(0x0), bytes32(0x0));
+        collectible = new MockCollectible();
+        treasure = new Treasure(address(collectible), bytes32(0x0));
+
+        // Add some assets to the treasure
+        erc20 = new MockERC20();
+        erc20.mint(address(treasure), 100 ether);
+        vm.deal(address(treasure), 100 ether);
+
+        // Give the owner the collectible
+        collectible.mint(user1);
     }
 
-    function test_CastOwner() public view {
-        assertEq(treasure.CAST_HASH(), bytes32(0x0));
+    function test_UserHasNft() public view {
+        assertEq(collectible.ownerOf(0), user1);
+    }
+
+    function test_OwnerCanWithdrawEth() public {
+        vm.prank(user1);
+        treasure.withdrawEth();
+        assertEq(address(user1).balance, 100 ether);
+    }
+
+    function test_RandoCantWithdrawEth() public {
+        vm.prank(user2);
+        vm.expectRevert(Treasure.NotCastOwner.selector);
+        treasure.withdrawEth();
+    }
+
+    function test_OwnerCanWithdrawErc20() public {
+        vm.prank(user1);
+        treasure.withdrawErc20(erc20, user1, 100 ether);
+        assertEq(erc20.balanceOf(user1), 100 ether);
+    }
+
+    function test_RandoCantWithdrawErc20() public {
+        vm.prank(user2);
+        vm.expectRevert(Treasure.NotCastOwner.selector);
+        treasure.withdrawErc20(erc20, user1, 100 ether);
+    }
+
+    function test_OwnerCanWithdrawErc20ToOtherAddress() public {
+        vm.prank(user1);
+        treasure.withdrawErc20(erc20, user2, 100 ether);
+        assertEq(erc20.balanceOf(user2), 100 ether);
+    }
+
+    function test_RandoCantWithdrawErc20ToOtherAddress() public {
+        vm.prank(user2);
+        vm.expectRevert(Treasure.NotCastOwner.selector);
+        treasure.withdrawErc20(erc20, user2, 100 ether);
+    }
+}
+
+contract MockCollectible is ERC721 {
+    constructor() ERC721("MockCollectible", "MC") {}
+
+    // Mint tokenId 0 to the owner, which would be the id for cast hash 0x0
+    function mint(address to) public {
+        _mint(to, 0);
+    }
+}
+
+contract MockERC20 is ERC20 {
+    constructor() ERC20("MockERC20", "ME") {}
+
+    function mint(address to, uint256 amount) public {
+        _mint(to, amount);
     }
 }
